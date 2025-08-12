3a347fe996cef97b70680e7747de99ba2b6f0944
MJK-One <hanelejss012@naver.com>
2025-08-12 18:58:37 +0900

    프로젝트 초기 커밋

---
+++ src/main/java/com/example/popularcommuity/PopularCommuityApplication.java
@@ -0,0 +1,13 @@
+package com.example.popularcommuity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopularCommuityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopularCommuityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/popularcommuity/PopularCommuityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popularcommuity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopularCommuityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
